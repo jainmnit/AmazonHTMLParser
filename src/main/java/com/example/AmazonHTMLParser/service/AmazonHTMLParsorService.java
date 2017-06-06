46cf6129388f481230f37ee35b717bae1fcfe0bf
Neeraj <neerajjain.jain@gmail.com>
2017-06-06 18:35:56 +0530

    Added XPath

--- src/main/java/com/example/AmazonHTMLParser/service/AmazonHTMLParsorService.java
+++ src/main/java/com/example/AmazonHTMLParser/service/AmazonHTMLParsorService.java
@@ -2,10 +2,11 @@ package com.example.AmazonHTMLParser.service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,10 +18,12 @@ import org.springframework.stereotype.Service;
 import com.example.AmazonHTMLParser.model.ProductDetails;
 import com.example.AmazonHTMLParser.model.SellerOfferDetail;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSpan;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+
+import us.codecraft.xsoup.Xsoup;
 
 @Service
 public class AmazonHTMLParsorService {
@@ -37,6 +40,7 @@ public class AmazonHTMLParsorService {
 		Element brandNameEle = doc.select("a#brand").first();
 		Element ASINEle = doc.select("input[id=ASIN]").first();
 		Element priceElement = doc.select("span#priceblock_ourprice").first();
+		String hrefSeller= Xsoup.compile("//div[@id='olp_feature_div']/div/span/a/@href").evaluate(doc).get();
 		productDetails.setName(productTitleEle.html());
 		productDetails.setBrand(brandNameEle.html());
 		productDetails.setASIN(ASINEle.attr("value"));
@@ -54,26 +58,13 @@ public class AmazonHTMLParsorService {
         final HtmlForm form1 = (HtmlForm) page.getForms().get(1);
         final HtmlSubmitInput button = form1.getInputByName("submit.add-to-cart");
         final HtmlPage page2 = button.click();
-        DomElement elementById = page2.getElementById("huc-v2-order-row-center-inner");
-        String nodeContent = elementById.getTextContent().trim();
-        System.out.println("mine is"+nodeContent);
-        System.out.println("mine length is"+nodeContent.length());
-        //String newContent = nodeContent.substring(nodeContent.indexOf("Cart subtotal (1 item):")+"Cart subtotal (1 item):".length()+3, nodeContent.length());
-        String newContent = stripNonDigits(nodeContent);
-        productDetails.setIncartprice("$"+newContent.trim());
+        final HtmlSpan elementById = (HtmlSpan)page2.getByXPath("//div[@id='huc-v2-order-row-center-inner']/div/div/div/div/span/span").get(1);
+        productDetails.setIncartprice(elementById.getTextContent());
 		}catch(Exception e){
 			productDetails.setIncartprice(priceElement.html());
         	logger.error("Unable to parse URL from sending to next page: "+url,e);
         }
-		int pageNumber = 1;
-		while(true){
-			
-			boolean endOfResults= getSellerInfo(productDetails.getASIN(),pageNumber,sellerOfferDetails);
-			if(endOfResults){
-				break;
-			}
-			pageNumber++;
-		}
+		getSellerInfo(hrefSeller,sellerOfferDetails);
 		}catch(Exception e){
 			logger.error("Unable to parse URL: "+url,e);
 		}
@@ -81,101 +72,70 @@ public class AmazonHTMLParsorService {
 		return productDetails;
 	}
 	
-	private boolean getSellerInfo(String asin, int pageNumber, List<SellerOfferDetail> sellerOfferDetails) throws IOException {
-		SellerOfferDetail sellerOfferDetail;
-		boolean endOfResults = false;
-		String sellerOfferURL = "https://www.amazon.com/gp/offer-listing/"+asin;
-		int startIndex = pageNumber*10;
-		String sellerOfferPageURL = sellerOfferURL+"/ref=olp_page_"+pageNumber+"?ie=UTF8&startIndex="+startIndex;
+	private void getSellerInfo(String sellerOfferPageURL, List<SellerOfferDetail> sellerOfferDetails) throws IOException {
+		sellerOfferPageURL = "https://www.amazon.com"+sellerOfferPageURL;
 		Document sellersURLDoc = Jsoup.connect(sellerOfferPageURL).get();
 		System.out.println(sellerOfferPageURL);
+		Elements divElementsForNext =  Xsoup.compile("//li[contains(@class, 'a-last') and contains(@class, 'a-disabled')]").evaluate(sellersURLDoc).getElements();
 		
+		String nextPage = Xsoup.compile("//li[@class='a-last']/a/@href").evaluate(sellersURLDoc).get();
+		getDetails(sellerOfferDetails, sellersURLDoc);
+		
+		if(divElementsForNext != null && divElementsForNext.size() >0){
+			return;
+		}else{
+			if(StringUtils.isEmpty(nextPage)){
+				return;
+			}else{
+				getSellerInfo(nextPage,sellerOfferDetails);
+			}
+		}
+	}
+
+	private void getDetails(List<SellerOfferDetail> sellerOfferDetails, Document sellersURLDoc) {
+		SellerOfferDetail sellerOfferDetail;
 		Elements divElements = sellersURLDoc.select("div[role='row']");
-		Elements divElementsForNext =  sellersURLDoc.getElementsByClass("a-last");
 		
 		
+		sellersURLDoc.getElementsByClass("a-last");
 		
 		for(Element divElementForSeller:divElements){
 			sellerOfferDetail = new SellerOfferDetail();
-			
 			Elements gridElements = divElementForSeller.select("div[role='gridcell']");
 			for(Element gridElementForSeller:gridElements){
-				Element spanTagForSeller = gridElementForSeller.select("span").first();
-				String classAttr = spanTagForSeller.attr("class");
-				//System.out.println(classAttr);
-				if(classAttr.contains("Price")){
-					String priceSeller = spanTagForSeller.html();
+				String priceSeller= Xsoup.compile("//span[contains(@class, 'Price')]/text()").evaluate(gridElementForSeller).get();
+				String cnditionSeller= Xsoup.compile("//span[contains(@class, 'Condition')]/text()").evaluate(gridElementForSeller).get();
+				String sellerName= Xsoup.compile("//h3[contains(@class, 'Seller')]/span/a/text()").evaluate(gridElementForSeller).get();
+				String hrefSeller= Xsoup.compile("//h3[contains(@class, 'Seller')]/span/a/@href").evaluate(gridElementForSeller).get();
+				if(StringUtils.isNoneEmpty(priceSeller)){
+					sellerOfferDetails.add(sellerOfferDetail);
 					sellerOfferDetail.setPrice(priceSeller);
 					System.out.println(priceSeller);
 				}
-				else if(classAttr.contains("Condition")){
-					String condition = spanTagForSeller.html();
-					sellerOfferDetail.setCondition(condition);
-					System.out.println(condition);
-				}
-				else if(classAttr.contains("a-size-medium")){
-					Element anchorTagForSeller = spanTagForSeller.child(0);
-					sellerOfferDetail.setSellerName(anchorTagForSeller.html());
-					String sellerId = anchorTagForSeller.attr("href").substring(anchorTagForSeller.attr("href").indexOf("seller")+7);
-					if(sellerId !=null){
-						sellerOfferDetails.add(sellerOfferDetail);
-					}
-					sellerOfferDetail.setSellerId(sellerId);
-					System.out.println(sellerId);
+				else if(StringUtils.isNoneEmpty(cnditionSeller)){
+					sellerOfferDetail.setCondition(cnditionSeller);
+					System.out.println(cnditionSeller);
+				}else if(StringUtils.isNoneEmpty(sellerName)){
+					System.out.println(sellerName);
+					sellerOfferDetail.setSellerName(sellerName);
+					sellerOfferDetail.setSellerId(getQueryMap(hrefSeller).get("seller"));
 				}
 			}
 		}
-		
-		if(divElementsForNext != null && divElementsForNext.first() != null){
-			String divElementsForNextClassName = divElementsForNext.first().attr("class");
-			System.out.println("divElementsForNextClassName is"+divElementsForNextClassName);
-			if(divElementsForNextClassName.contains("a-disabled")){
-				System.out.println("Comes here");
-				endOfResults = true;
-			}
-		}else{
-			endOfResults = true;
-		}
-		
-		return endOfResults;
 	}
 	
 	
-	public static String stripNonDigits(String input){
-		String returnString = "";
-		Pattern p = Pattern.compile("-?\\d+");
-		Matcher m = p.matcher(input);
-		int i = 0;
-		while (m.find()) {
-			
-		  System.out.println("Group info"+m.group());
-		 if(i==1){
-			 returnString = m.group();
-		 }
-		 if(i==2){
-			 returnString +="."+m.group();
-		 }
-		 i++;
-		}
-		return returnString;
-		
-		/*Pattern pattern = Pattern.compile("([0-9]*)");
-		Matcher matcher = pattern.matcher(input);
-		boolean matchFound = matcher.find();
-		
-		if (matchFound) {
-		    System.out.println("GRoup is"+matcher.group());
-		    if(matcher.group(1) != null){
-		    	  System.out.println("GRoup1 is"+matcher.group(1));
-		    	returnString = matcher.group(0)+"."+matcher.group(1);
-		    }else{
-		    	returnString = matcher.group(0);
-		    }
-		    return returnString;
-		}else{
-			return returnString;
-		}*/
-   
-}
+	public static Map<String, String> getQueryMap(String query)  
+	{  
+	    String[] params = query.split("&");  
+	    Map<String, String> map = new HashMap<String, String>();  
+	    for (String param : params)  
+	    {  
+	        String name = param.split("=")[0];  
+	        String value = param.split("=")[1];  
+	        map.put(name, value);  
+	    }  
+	    return map;  
+	}
 	
 }
